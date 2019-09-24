abfd5ff1538fdfde189e2b9fa16e09424313490c
DianRP43 <55683288+DianRP43@users.noreply.github.com>
2019-09-24 15:20:30 +0700

    Add files via upload
    PBO1 = IF-01

---
+++ PBO1-10118043-Latihan12-Aritmatika/src/index.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class index {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a, b, c, d;
+        a = 10; b = 20; c = 25; d = 25;
+        
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("a-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("d++ = " + (++d));
+    }
+    
+}
